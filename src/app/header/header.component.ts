c3f689f72c5e4d6b7b24abb6a3d3e30939bf61ea
Karol Cislowski <cislowski.karol@hotmail.com>
2021-02-03 18:48:29 +0100

    simple navigation (ngIf)

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -12,5 +12,11 @@ export class HeaderComponent implements OnInit {
   ngOnInit(): void {
 
   }
-    collapsed = true;
+  @Output() featureSelected = new EventEmitter<string>()
+
+  onSelect(feature: string) {
+    this.featureSelected.emit(feature)
+  }
+
+  collapsed = true;
 }
