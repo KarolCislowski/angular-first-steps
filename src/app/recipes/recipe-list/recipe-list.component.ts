913dea3428d0b6552a2856fa130be3c0581ea317
Karol Cislowski <cislowski.karol@hotmail.com>
2021-02-06 20:05:25 +0100

    moving array of recipes to the service

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,39 +1,23 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core'
 import { Recipe } from '../recipe.model'
+import { RecipeService } from '../recipe.service'
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [
-    new Recipe(
-      'Alghoul decoction',
-      'Adrenaline Points are generated more quickly than normal until the first successful enemy attack. 50% Adrenaline Point gain',
-      'https://images.unsplash.com/photo-1610706936473-6b9667f928e7?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80'
-    ),
-    new Recipe(
-      'Alghoul decoction',
-      'Adrenaline Points are generated more quickly than normal until the first successful enemy attack. 50% Adrenaline Point gain',
-      'https://images.unsplash.com/photo-1610706936473-6b9667f928e7?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80'
-    ),
-    new Recipe(
-      'Alghoul decoction',
-      'Adrenaline Points are generated more quickly than normal until the first successful enemy attack. 50% Adrenaline Point gain',
-      'https://images.unsplash.com/photo-1610706936473-6b9667f928e7?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80'
-    )
-  ]
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+  recipes: Recipe[]
 
-  @Output() recipeWasSelected = new EventEmitter<Recipe>()
-
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit(): void {
+    this.recipes = this.recipeService.getRecipes()
   }
 
   onRecipeSelected(recipe: Recipe) {
     this.recipeWasSelected.emit(recipe)
   }
-
 }
---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,25 @@
+import { Recipe } from './recipe.model'
+
+export class RecipeService {
+  private recipes: Recipe[] = [
+    new Recipe(
+      'Alghoul decoction',
+      'Adrenaline Points are generated more quickly than normal until the first successful enemy attack. 50% Adrenaline Point gain',
+      'https://images.unsplash.com/photo-1610706936473-6b9667f928e7?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80'
+    ),
+    new Recipe(
+      'Alghoul decoction',
+      'Adrenaline Points are generated more quickly than normal until the first successful enemy attack. 50% Adrenaline Point gain',
+      'https://images.unsplash.com/photo-1610706936473-6b9667f928e7?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80'
+    ),
+    new Recipe(
+      'Alghoul decoction',
+      'Adrenaline Points are generated more quickly than normal until the first successful enemy attack. 50% Adrenaline Point gain',
+      'https://images.unsplash.com/photo-1610706936473-6b9667f928e7?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80'
+    ),
+  ]
+
+  getRecipes() {
+    return this.recipes.slice()
+  }
+}
