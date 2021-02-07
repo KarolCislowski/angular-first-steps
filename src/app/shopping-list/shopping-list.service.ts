d346462fdda955e6b01324aa85d92e97cec3e2c0
Karol Cislowski <cislowski.karol@hotmail.com>
2021-02-07 16:42:08 +0100

    ingredients service

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,21 @@
+import { EventEmitter } from "@angular/core"
+import { Ingredient } from "../shared/ingredient.model"
+
 export class ShoppingListService {
-  
+  ingredientsChanged = new EventEmitter<Ingredient[]>()
+  private ingredients: Ingredient[] = [
+    new Ingredient('Dwarven spirit', 1),
+    new Ingredient('Lesser red mutagen', 5),
+    new Ingredient('Alghoul bone marrow', 1),
+    new Ingredient('Buckthorn', 1)
+  ]
+
+  getIngredients() {
+    return this.ingredients.slice()
+  }
+
+  addIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient)
+    this.ingredientsChanged.emit(this.ingredients.slice())
+  }
 }
